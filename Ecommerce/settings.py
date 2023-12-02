910b10060e7389ba552c4751d429b1781186966e
Boudjidj-Bilal <100086377+Boudjidj-Bilal@users.noreply.github.com>
2023-12-02 21:52:00 +0100

    on teste avec une base de donnees postgresqs

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -84,10 +84,21 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fzudipus',
+        'USER': 'fzudipus',
+        'PASSWORD': 'F5fVZq0C8I5d-B9lz7-4_vYM4C36BNqm',
+        'HOST': 'snuffleupagus.db.elephantsql.com',
+        'PORT': '',
     }
 }
 
